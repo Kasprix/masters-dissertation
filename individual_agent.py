7cffa6220a8439e019f8dd310b53d61c81782964
Reece <65017864+Kasprix@users.noreply.github.com>
2022-07-02 16:25:18 +0100

    adding argument feature

--- individual_agent.py
+++ individual_agent.py
@@ -21,11 +21,16 @@ class Agent:
   # Checks if argument is in or out
   def get_argument_state(agent):
     if len(agent.attacked_by) > 0:
+      print("Test", agent.attacked_by)
       return "Out"
     elif len(agent.attacked_by) == 0:
       return "In"
     else:
       return "Error"
+
+  def argument_state(agent):
+    for x in agent.attacked_by:
+      print(str_to_class(x))
 
   def attack(agent, obj_name, victim):
 
@@ -39,6 +44,8 @@ class Agent:
         victim_object.attacked_by.append(agent.name)  
     else:
         print("shit")
+
+  
 
 
 def create_agent(agent_name, state):
@@ -58,8 +65,15 @@ print("\n" + p1.name, "is attacking:")
 print(p1.attacking)
 print("\n" + p2.name, "is attacking:")
 print(p2.attacking)
+print("State for Reece is", p1.get_argument_state())
+
+print(p1.argument_state())
+
+
 
 print("\n" + p1.name, "is attacked by:")
 print(p1.attacked_by)
 print("\n" + p2.name, "is attacked by:")
 print(p2.attacked_by)
+
+print(p2.argument_state())
