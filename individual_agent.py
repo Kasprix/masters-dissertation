e5853054a603b942ce734decc8257f67906790ce
Reece <65017864+Kasprix@users.noreply.github.com>
2022-07-15 13:53:14 +0100

    Update individual_agent.py

--- individual_agent.py
+++ individual_agent.py
@@ -47,13 +47,14 @@ class Agent:
     else:
         print("shit")
 
+
   def attack_agent(agent, victim):
 
-        agent.attacking.append(victim.name)
+        agent.attacking.append(victim)
 
         print(agent.name, "is now hurling rocks at", victim.name)
 
-        victim.attacked_by.append(agent.name)  
+        victim.attacked_by.append(agent)  
 
 
 def create_agent(agent_name):
@@ -66,26 +67,9 @@ p2 = create_agent("Carly")
 
 p2.attack("p1", p1.name)
 
-'''
-p1.attack("p2", p2.name)
-print("\n" + p1.name, "is attacking:")
-print(p1.attacking)
-print("\n" + p2.name, "is attacking:")
-print(p2.attacking)
-print("State for Reece is", p1.get_argument_state())
 
-print(p1.argument_state())
-
-
-
-print("\n" + p1.name, "is attacked by:")
-print(p1.attacked_by)
-print("\n" + p2.name, "is attacked by:")
-print(p2.attacked_by)
-'''
 
 # Makes List of Agents to create based on variable input by the user
-  
 def create_framework(variable):
   arguments_to_take = list(string.ascii_lowercase[:variable])
   agent_list = []
@@ -99,20 +83,20 @@ def create_framework(variable):
 
     random_value = random.randint(0,9)
     random_threshhold = random.randint(0,9)
-    arguments_exclusing_value = [x for x in agent_list if x != names]
+    arguments_excluding_value = [x for x in agent_list if x != names]
 
     # If the value is below the generated threshold, then the agent will attack a random agent
     if random_value < random_threshhold:
-      victim = random.choice(arguments_exclusing_value)
+      victim = random.choice(arguments_excluding_value)
       print(str(names.name), "Attacks", victim.name)
       names.attack_agent(victim)
       # Checks and gives 30% odds of the argument choosing to attack yet another argument
       if random.randint(0,9) < 3:
-        arguments_exclusing_value = [x for x in arguments_exclusing_value if x != victim]
-        otherVictim = random.choice(arguments_exclusing_value)
+        arguments_excluding_value = [x for x in arguments_excluding_value if x != victim]
+        otherVictim = random.choice(arguments_excluding_value)
         names.attack_agent(otherVictim)
     else:
-      print(names.name, "Leaves", random.choice(arguments_exclusing_value).name)
+      print(names.name, "Leaves", random.choice(arguments_excluding_value).name)
 
 
     print(names.attacking)
@@ -120,5 +104,43 @@ def create_framework(variable):
 
   return agent_list
 
-
+# Initialises Framework with variable of arguments set by user
 list_of_objects = create_framework(20)
+
+def grounded_initial_labellings(framework):
+  for v in framework:
+    if len(v.attacked_by) == 0:
+      v.state = "In"
+    for x in v.attacked_by:
+      print(v.name, 'is attacked by', x.name)
+
+    print(v.name, "Current state is ", v.state)
+
+
+  for v in framework:
+    for x in v.attacked_by:
+      # TODO find out if Out is called if it's being attacked by one or more arguments, for now it's coded on the assumption it means one attacker
+      if x.state == "In" and len(v.attacked_by) == 1 :
+        v.state == "Out"
+        print(v.name, "Changed to", v.state)
+
+  return framework
+
+
+grounded_framework = grounded_initial_labellings(list_of_objects)
+
+# TODO Make a class possibly for the two users (Pro & Opp)
+
+'''
+TODO Grounded Rules
+IN For all unattacked arguments
+OUT to any argument that is attacked and just made IN
+IN to all who are attacked by only OUTS
+ANY Remaining arguments are deemed UNDEC
+
+TODO Game tree function that constructs the gametree for all possible outcomes from playing that game
+The OPP can reference the gametree to decide its next move
+
+TODO Function that finds and saves the winning strategy
+
+'''
